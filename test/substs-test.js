06d5799a0664c851a10865997c28b20bb981b3e5
Dmitry Filatov <dfilatov@yandex-team.ru>
2012-11-01 14:36:26 +0400

    performance improvements

--- test/substs-test.js
+++ test/substs-test.js
@@ -3,6 +3,7 @@ module.exports = [
     { path : '.books{.idx === $idx}.idx', substs : { idx : "1" }, res : []},
     { path : '.books{.idx == $idx}.idx', substs : { idx : "1" }, res : [1]},
     { path : '.books{.idx === $idx}.idx', substs : { idx : [1, 2] }, res : [1, 2]},
+    { path : '.books{.oldPrices === $prices}.idx', substs : { prices : [10, 15] }, res : [0, 1, 3]},
     { path : '.books[$idx].idx', substs : { idx : 1 }, res : [1]},
     { path : '.books[$idxFrom:$idxTo].idx', substs : { idxFrom : 1, idxTo : 3 }, res : [1, 2]}
 ];
