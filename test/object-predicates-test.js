2a5287042949a1d0052b52a11af92c750d26bf0b
Dmitry Filatov <dfilatov@yandex-team.ru>
2012-11-16 14:21:16 +0400

    0.2.1

--- test/object-predicates-test.js
+++ test/object-predicates-test.js
@@ -1,12 +1,12 @@
 module.exports = [
-    { path : '.{.{.}}', data : 'test', res : 'test'},
-    { path : '.{.data}', data : { data : 0 }, res : { data : 0 }},
-    { path : '.{.data}', data : { data : false }, res : { data : false }},
+    { path : '.{.{.}}', data : 'test', res : ['test']},
+    { path : '.{.data}', data : { data : 0 }, res : [{ data : 0 }]},
+    { path : '.{.data}', data : { data : false }, res : [{ data : false }]},
     { path : '.{.data}', data : [{ data : false }], res : [{ data : false }]},
-    { path : '.info{.description}', res : { description : 'books about javascript', content : 'content' }},
-    { path : '.info{.noexists}', res : undefined },
+    { path : '.info{.description}', res : [{ description : 'books about javascript', content : 'content' }]},
+    { path : '.info{.noexists}', res : []},
     { path : '.books{.favorite}.idx', res : [1, 2]},
     { path : '.books{.author.name}.idx', res : [0, 1, 2, 3, 4]},
     { path : '.books{.description.notes}.idx', res : [3]},
-    { path : '.books{.author.noexists}.idx', res : undefined },
+    { path : '.books{.author.noexists}.idx', res : []}
 ];
--- test/undefined-and-null-test.js
+++ test/undefined-and-null-test.js
@@ -1,7 +1,7 @@
 module.exports = [
-    { path : '.', data : null, res : null },
+    { path : '.', data : null, res : [null]},
     { path : '.', data : [null, { test : [null] }], res : [null, { test : [null] }]},
-    { path : '.', data : undefined, res : undefined },
+    { path : '.', data : undefined, res : [undefined]},
     { path : '.', data : [undefined, { test : [undefined] }], res : [undefined, { test : [undefined] }]},
     { path : '.item.idx', data : [{ item : { idx : 0 }}, null], res : [0]},
     { path : '..item..idx', data : [{ item : { idx : 0 }}, null], res : [0]},
