c33cdcb47fbddf113142c54e1d79f9d515b9cda3
Dmitry Filatov <dfilatov@yandex-team.ru>
2012-11-22 12:25:33 +0400

    Add tests for parsing errors (#12)

---
+++ test/parser-errors-test.js
@@ -0,0 +1,11 @@
+module.exports = [
+    { path : '', message : 'Unexpected end of path' },
+    { path : 'id', message : 'Unexpected token "id"' },
+    { path : '{}', message : 'Unexpected token "{"' },
+    { path : '[]', message : 'Unexpected token "["' },
+    { path : '.{.a > }', message : 'Unexpected token "}"' },
+    { path : '.{.a >', message : 'Unexpected end of path' },
+    { path : '.(.a || .b)', message : 'Unexpected token "||"' },
+    { path : '.[:2:]', message : 'Unexpected token ":"' },
+    { path : '.[1:2:3]', message : 'Unexpected token ":"' }
+];
--- test/test.js
+++ test/test.js
@@ -1,5 +1,23 @@
 var jspath = require('../index'),
-    testData = require('./data'),
+    testCounter = 0;
+
+// parser tests
+require('./parser-errors-test').forEach(function(desc) {
+    exports[++testCounter + '. ' + desc.path] = function(test) {
+        test.throws(
+            function() {
+                jspath.apply(desc.path, {});
+            },
+            function(err) {
+                return err.message === desc.message;
+            });
+        test.done();
+    };
+});
+
+
+// applying tests
+var testData = require('./data'),
     testDataCopy = JSON.parse(JSON.stringify(testData)),
     tests = [
             'path',
@@ -20,8 +38,9 @@ var jspath = require('../index'),
             },
             []);
 
-tests.forEach(function(testItem, i) {
-    exports[i + 1 + '. ' + testItem.path] = function(test) {
+
+tests.forEach(function(testItem) {
+    exports[++testCounter + '. ' + testItem.path] = function(test) {
         test.deepEqual(
             jspath.apply(
                 testItem.path,
@@ -32,7 +51,8 @@ tests.forEach(function(testItem, i) {
     };
 });
 
-exports['immutable data'] = function(test) {
+// immutable test
+exports[++testCounter + '. immutable data'] = function(test) {
     test.deepEqual(testData, testDataCopy);
     test.done();
 };
