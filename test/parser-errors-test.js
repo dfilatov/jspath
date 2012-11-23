7e7b7f1ca73defe88b751825eeb4a10f42c57f44
Dmitry Filatov <dfilatov@yandex-team.ru>
2012-11-23 17:44:08 +0400

    add some tests

--- test/parser-errors-test.js
+++ test/parser-errors-test.js
@@ -1,11 +1,14 @@
 module.exports = [
     { path : '', message : 'Unexpected end of path' },
     { path : 'id', message : 'Unexpected token "id"' },
+    { path : '.#', message : 'Unexpected token "#"' },
+    { path : '.[.] id', message : 'Unexpected token "id"' },
     { path : '{}', message : 'Unexpected token "{"' },
     { path : '[]', message : 'Unexpected token "["' },
     { path : '.{.a > }', message : 'Unexpected token "}"' },
     { path : '.{.a >', message : 'Unexpected end of path' },
     { path : '.(.a || .b)', message : 'Unexpected token "||"' },
     { path : '.[:2:]', message : 'Unexpected token ":"' },
-    { path : '.[1:2:3]', message : 'Unexpected token ":"' }
+    { path : '.[1:2:3]', message : 'Unexpected token ":"' },
+    { path : '.{.2.3}', message : 'Unexpected token "."' }
 ];
