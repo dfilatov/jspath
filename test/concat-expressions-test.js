8ee361ba11b16ea3ecca810c032820e5a36cccde
Dmitry Filatov <dfilatov@yandex-team.ru>
2015-01-10 00:54:31 +0300

    add ability to concat_expr be top-level expression (close #29)

--- test/concat-expressions-test.js
+++ test/concat-expressions-test.js
@@ -1,5 +1,11 @@
 module.exports = [
     { path : '.(.name | .booksCount)', res : ['books and authors', 5]},
     { path : '.books(.idx{. === 1} | .idx{. === 2})', res : [1, 2]},
-    { path : '.books(.idx{. === 3} | .author[0].name)', res : [3, 'Robert C. Martin']}
+    { path : '.books(.idx{. === 3} | (((.author[0].name)) | .author[0].name))', res : [3, 'Robert C. Martin', 'Robert C. Martin']},
+    { path : '.name | .booksCount', res : ['books and authors', 5]},
+    { path : '(.name)', res : ['books and authors']},
+    { path : '(.name | .booksCount)', res : ['books and authors', 5]},
+    { path : '.name | (.booksCount | .booksCount)', res : ['books and authors', 5, 5]},
+    { path : '(.name | .booksCount)[0]', res : 'books and authors'},
+    { path : '.name | .booksCount[0]', res : ['books and authors', 5]}
 ];
