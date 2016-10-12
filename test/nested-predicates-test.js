370889960cdde9c437d6b472fddc5b86f619a817
Kevin Prichard <kprichard@castlighthealth.com>
2016-10-11 18:30:03 -0700

    Typo and test style consistency.

--- test/nested-predicates-test.js
+++ test/nested-predicates-test.js
@@ -1,5 +1,5 @@
 module.exports = [
-    { path : '.books..{.price<20 && (.author.name==="Nicholas C. Zakas" || .author.name*=="Robert C. Martin")}.name',
+    { path : '.books..{.price<20 && (.author.name*=="Zakas" || .author.name*=="Martin")}.name',
       res : ["Clean Code", "Maintainable JavaScript"]},
     { path : '.books..{.price<20 && .author{.name*=="Zakas" || .name*=="Martin"}}.name',
       res : ["Clean Code", "Maintainable JavaScript"]},
